d1a2213f23388885c097d41823e6d62ac1bacd17
Saba <saba.ansari44647@gmail.com>
2025-03-24 13:44:53 +0330

    change picture

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Brands: React.FC = () => {
     const brands = [
-        { id: 1, name: 'برند ۱', image: 'https://hirsagol.com/wp-content/uploads/2019/03/red-rose.jpg', link: '#' },
-        { id: 2, name: 'برند ۲', image: 'https://hirsagol.com/wp-content/uploads/2019/03/red-rose.jpg', link: '#' },
-        { id: 3, name: 'برند ۳', image: 'https://hirsagol.com/wp-content/uploads/2019/03/red-rose.jpg', link: '#' },
-        { id: 4, name: 'برند ۴', image: 'https://hirsagol.com/wp-content/uploads/2019/03/red-rose.jpg', link: '#' },
-        { id: 5, name: 'برند ۵', image: 'https://hirsagol.com/wp-content/uploads/2019/03/red-rose.jpg', link: '#' },
+        { id: 1, name: 'برند ۱', image: 'https://dkstatics-public.digikala.com/digikala-brands/2c36ceb629598fa31b9b321fcd4e47773bd93ef7_1649498016.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80', link: '#' },
+        { id: 2, name: 'برند ۲', image: 'https://dkstatics-public.digikala.com/digikala-brands/1313.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80', link: '#' },
+        { id: 3, name: 'برند ۳', image: 'https://dkstatics-public.digikala.com/digikala-brands/3960.jpg?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80', link: '#' },
+        { id: 4, name: 'برند ۴', image: 'https://dkstatics-public.digikala.com/digikala-brands/960dc4b74e21a9d33d594a161386179b26b3a68a_1606823783.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80', link: '#' },
+        { id: 5, name: 'برند ۵', image: 'https://dkstatics-public.digikala.com/digikala-brands/5a10ae95eb42fc96906c8062563b28062668d418_1722238640.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80', link: '#' },
     ];
 
     return (
