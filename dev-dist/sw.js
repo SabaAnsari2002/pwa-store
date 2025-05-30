5b097cf97d55607de090ec5667575df17f16eeee
pooria159 <pooriarahimi159@gmail.com>
2025-05-31 02:36:04 +0330

    [Fix]: fix API to create products

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-3d1c42bc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ck3m3opvur8"
+    "revision": "0.7gdq1aqidjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
